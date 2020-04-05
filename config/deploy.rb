9ad3ecab07e9e00c4326270610bf3e9b06fb4e16
Dongseop Kwon <dongseop@gmail.com>
2020-04-05 20:53:19 +0900

    remove lightbox from index page

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.12.0"
 
 set :application, "pubqrator"
 set :repo_url, "git@gitlab.com:ranzzy/pubqrator.git"
