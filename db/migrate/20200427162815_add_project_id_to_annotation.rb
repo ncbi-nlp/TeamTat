a055dfea04e50a6b456290d9add56d8342e3b518
Dongseop Kwon <dongseop@gmail.com>
2020-04-28 01:57:16 +0900

    change migration to reduce deploy load

--- db/migrate/20200427162815_add_project_id_to_annotation.rb
+++ db/migrate/20200427162815_add_project_id_to_annotation.rb
@@ -1,10 +1,5 @@
 class AddProjectIdToAnnotation < ActiveRecord::Migration[5.2]
   def change
     add_reference :annotations, :project, foreign_key: true
-
-    Annotation.all.each do |a|
-      a.project = a.document.project
-      a.save
-    end
   end
 end
