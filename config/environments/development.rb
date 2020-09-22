7a79cbe39c10a6b5dfc39f8c084bcef397abad7f
khalil09 <khalilcarsten@gmail.com>
2020-09-22 17:30:53 -0300

    relative host added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   config.read_encrypted_secrets = true
-  config.action_controller.default_url_options = { host: '164.41.76.201:80/teamtat' }
+  config.action_controller.default_url_options = { host: '164.41.76.201', port: '80' }
+  config.relative_url_root = "/teamtat"
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
