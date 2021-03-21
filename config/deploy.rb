cf09a43b7130a4e47029f879710aa724a9710638
Dongseop Kwon <dongseop@gmail.com>
2021-03-22 00:54:12 +0900

    bump to capistrano 3.16.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.12.0"
+lock "~> 3.16.0"
 
 set :application, "pubqrator"
 set :repo_url, "git@github.com:ncbi-nlp/TeamTat.git"
